5e23aa589af816e8afdbd3336f9f79978d35793d
Benknightdark <moon791017@gmail.com>
2018-04-18 15:12:17 +0800

    add bbbbbb

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,7 +12,7 @@ namespace AutoCadFileViewer
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-
+    //bbbbbb
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
