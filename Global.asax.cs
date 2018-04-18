f3ff2a01a5bbd3a563338065c96ba6da5be77e9d
BenKung <moon791017@gmail.com>
2018-04-18 15:13:03 +0800

    update bbdfsdafasdfa

--- Global.asax.cs
+++ Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using WebCAD;
-//aaa
+//aaabbbbbbb
 namespace AutoCadFileViewer
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
